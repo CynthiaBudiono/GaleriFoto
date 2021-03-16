1669d970a1e750b1baf9d83da9988900bb54f1db
CynthiaBudiono <c14180210@john.petra.ac.id>
2021-03-16 23:24:22 +0800

    buat game sederhana

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   constructor() {}
-
+  acak:Number;
+  txttebak:Number;
+  cek:boolean;
+  tebak(){
+    // alert("masuk");
+    this.acak = Math.floor(Math.random()*20)+1;
+    alert(this.acak);
+    if(this.txttebak == null){
+      alert("harus diisi");
+    }
+    else {
+      if(this.txttebak == this.acak){
+        this.cek=true;
+      }
+      else{
+        this.cek=false;
+      }
+    }
+    this.txttebak=null;
+  }
 }
